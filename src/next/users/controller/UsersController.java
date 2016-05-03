9082e648d4a33f6d9d53c3cf37557234ea195748
Brizeli <eugene.brizeli@gmail.com>
2016-05-03 20:01:45 +0300

    Update UsersController.java

--- src/next/users/controller/UsersController.java
+++ src/next/users/controller/UsersController.java
@@ -5,11 +5,7 @@ import next.users.model.UsersHibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -26,7 +22,6 @@ public class UsersController {
     @RequestMapping({"/", "home"})
     String index() {
         return "index";
-//        return "forward:/index";
     }
 
     @RequestMapping(value = "/searchUsers")
@@ -79,63 +74,4 @@ public class UsersController {
         model.addAttribute("result", addResult);
         return "addUserForm";
     }
-//    @RequestMapping("/index1")
-//    public String setupForm(Map<String, Object> map) {
-//        User user = new User();
-//        map.put("user", user);
-//        map.put("userList", usersdb.getAllUsers());
-//        return "user";
-//    }
-//
-//    @RequestMapping(value = "/user.do", method = RequestMethod.POST)
-//    public String doActions(@ModelAttribute User user, BindingResult result, @RequestParam String action, Map<String, Object> map) {
-//        User userResult = new User();
-//        switch (action.toLowerCase()) {
-//            case "add":
-//                usersdb.addUser(user);
-//                userResult = user;
-//                break;
-//            case "edit":
-//                usersdb.edit(user);
-//                userResult = user;
-//                break;
-//            case "delete":
-//                usersdb.delete(user.getId());
-//                userResult = new User();
-//                break;
-//            case "search":
-//                User userSearch = usersdb.getUser(user.getId());
-//                userResult = userSearch != null ? userSearch : new User();
-//                break;
-//        }
-//        map.put("user", userResult);
-//        map.put("userList", usersdb.getAllUsers());
-//        return "user";
-//    }
-//    @RequestMapping("/")
-//    String home() {
-//        return "home";
-//    }
-//
-//    @RequestMapping(value = "addUserForm")
-//    String addUserForm() {
-//        return "addUserForm";
-//    }
-//
-//    @RequestMapping(value = "addUser", method = RequestMethod.POST)
-//    String addUser(String name, String age, String isAdm, Model model) {
-//        int uAge = Integer.parseInt(age);
-//        boolean adm = isAdm.equals("on") ? true : false;
-//        User user = new User(name, uAge, adm);
-//        boolean res = usersdb.addUser(user);
-//        model.addAttribute("result", res);
-//        return "home";
-//    }
-//
-//    @RequestMapping(value = "showUsers", method = RequestMethod.GET)
-//    String showUsers(Model model) {
-//        List<User> users = usersdb.getAllUsers();
-//        model.addAttribute("result", users);
-//        return "viewUsers";
-//    }
 }
